932c21b666d5061c1d64b2ececf28dd7d480933f
jiiiiiw <kijiwi1@naver.com>
2024-07-01 18:23:46 +0900

    :tada: feat: 프로젝트 구축

---
+++ src/main/java/yagujoahhaseyo/howoo/hitNstat/HitNstatApplication.java
@@ -0,0 +1,13 @@
+package yagujoahhaseyo.howoo.hitNstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitNstatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitNstatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yagujoahhaseyo/howoo/hitNstat/HitNstatApplicationTests.java
@@ -0,0 +1,13 @@
+package yagujoahhaseyo.howoo.hitNstat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitNstatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
